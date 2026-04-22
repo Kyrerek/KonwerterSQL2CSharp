3545dbecaeb9eebecf29737078619ba738aa3fcd
michal <mmasalski@student.agh.edu.pl>
2026-04-23 00:40:43 +0200

    added functional example

--- main/src/main/java/org/example/Main.java
+++ main/src/main/java/org/example/Main.java
@@ -3,18 +3,24 @@ package org.example;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
-import antlr.HelloLexer;
+
 import antlr.HelloParser;
+import antlr.HelloLexer;
 
 
 public class Main {
     public static void main(String[] args) {
-        String testStr = "hello world";
+        String testStr = """
+        SELECT * 
+        FROM klienci 
+        JOIN zamowienia ON klienci.id = zamowienia.id_klienta
+        WHERE zamowienia.ilosc > 2 AND klientci.id BETWEEN 10 and 100
+        ORDER BY klienci.nazwa DESC;
+        """.trim();
         HelloLexer lexer = new HelloLexer(CharStreams.fromString(testStr));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         HelloParser parser = new HelloParser(tokens);
-
-        ParseTree tree = parser.r();   // start rule
+        ParseTree tree = parser.query();
         System.out.println(tree.toStringTree(parser));
     }
 }
